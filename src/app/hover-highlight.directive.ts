ed1dd97b461e648f0a527d1af1bfc63fcddbd9cd
gedharizka <gedharizka@gmail.com>
2019-01-23 11:10:38 +0700

    Directive 2

--- src/app/hover-highlight.directive.ts
+++ src/app/hover-highlight.directive.ts
@@ -1,13 +1,27 @@
-import { Directive, ElementRef, Renderer2} from '@angular/core';
+import { Directive, ElementRef, Renderer2,HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[appHoverHighlight]'
 })
 export class HoverHighlightDirective /* implement OnInit */ {
 
+  @HostBinding('style.color') textColor: string;
+  @Input ('appHoverHighlight')highLightColor:{background:string, text:string}
+
   constructor (private elRef: ElementRef, private render: Renderer2) { }
 
-  ngOnInit(): void {
+  /*ngOnInit(): void {
     this.render.setStyle(this.elRef.nativeElement, 'background-color','salmon');
+  }*/
+
+  @HostListener("mouseover")mouseMasuk(evenData: Event){
+    this.render.setStyle (this.elRef.nativeElement,'background-color', this.highLightColor.background);
+    this.textColor = this.highLightColor.text;
   }
+
+  @HostListener ("mouseleave")mouseKeluar(evenData:Event){
+    this.render.setStyle(this.elRef.nativeElement,'background-color','transparent');
+    this.textColor='black';
+  }
+
 }
