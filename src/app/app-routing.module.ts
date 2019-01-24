7f1d39fdbb6fed26b7e833b3d20cc9028cae7c4c
gedharizka <gedharizka@gmail.com>
2019-01-24 09:49:08 +0700

    Routing Successful

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TemanComponent } from './teman/teman.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'home', component: HomeComponent },
+  {path : '404', component: PageNotFoundComponent },
+  {path : 'teman', component: TemanComponent },
+  {path : 'header', component: HeaderComponent },
+  {path : 'contact-us', component: ContactUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
